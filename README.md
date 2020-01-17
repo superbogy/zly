4afcd3263c8da46c168209508a9d73b7af6b8a6b
Tommy Wei <mulberry10th@gmail.com>
2020-01-18 00:26:56 +0800

    feat: add bin execute script

--- README.md
+++ README.md
@@ -1,3 +1,64 @@
-# zly
-zly is a yaml assembly. You can split a large yaml file into many fragments. It is very useful with writing a swagger API document.
+:dango: zly is a yaml assembly. You can split a large yaml file into many fragments.
+It is very useful with writing a swagger API document. 
 You can refer yaml piece from relative path or absolute path.
+Think about that when you try to write a swagger document with more that 100 apis, how large the yaml file will be. It is hard to edit and make giddy.
+
+### Install 
+
+`npm i -g zly`
+
+If you have not installed node.js env, here is a [binary file](https://github.com) with simple function. 
+
+
+### Get-started
+
+```
+Usage: index [options]
+
+Options:
+  -V, --version        output the version number
+  -o, --output <path>  put the result to file
+  -w, --workspace      yaml file root path
+  -f, --format <type>  format output style, yaml or json (default: "yaml")
+  -p, --pretty         pretty out put
+  -s, --server         start a swagger ui server
+  -e, --extname <ext>  extend name, split with comma
+  -h, --help           output usage information
+ ```
+ 
+ ### demo
+ 
+- import yaml file
+    `$ref: relative/path/some.yaml`
+- structure
+```
+├── definitions
+│   ├── Error.yaml
+│   ├── Pet.yaml
+│   └── index.yaml
+├── entry.yaml
+└── routers
+    ├── index.yaml
+    └── pets
+        ├── $id.yaml
+        └── list.yaml
+```
+The entry file refer two file from `definitions/index.yaml` and `routers/index.yaml`
+
+```
+paths:
+  $ref: routers/index.yaml
+components:
+  $ref: definitions/index.yaml
+```
+for routers/index.yaml:
+
+```
+/pets:
+  $ref: pets/list.yaml
+/pets/{petId}:
+  $ref: pets/$id.yaml
+
+```
+
+run with `zly -s entry.yaml`
---
+++ bin/index.js
@@ -0,0 +1,100 @@
+#!/usr/bin/env node
+
+const commander = require('commander');
+const fs = require('fs');
+const color = require('cli-color');
+const Yaml = require('yaml');
+const Zly = require('../lib/index');
+const watcher = require('chokidar');
+const open = require('open');
+const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const http = require('http');
+
+const monitor = {};
+const restart = (entry, workspace, extnames) => {
+  monitor.pending = true;
+  monitor.action = 'restart';
+  if (!monitor.server) {
+    const app = express();
+    app.use('/', swaggerUi.serve);
+    app.get('/', (req, res, next) => {
+      const zly = new Zly(entry, workspace, extnames);
+      data = zly.run();
+      return swaggerUi.setup(data)(req, res, next);
+    });
+    const server = http.createServer(app);
+    monitor.state = 'running';
+    monitor.pending = false;
+    server.listen('3300', '0.0.0.0');
+    server.on('listening', () => {
+      open('http://localhost:3300/', { background: true });
+    });
+    monitor.server = server;
+  }
+};
+
+commander.version('0.0.1')
+  .option('-o, --output <path>', 'put the result to file')
+  .option('-w, --workspace', 'yaml file root path')
+  .option('-f, --format <type>', 'format output style, yaml or json', 'yaml')
+  .option('-p, --pretty', 'pretty out put')
+  .option('-s, --server', 'start a swagger ui')
+  .option('-e, --extname <ext>', 'extend name, split with comma')
+  .parse(process.argv);
+
+try {
+  const entry = commander.args[0];
+  if (!entry) {
+    console.error(color.red('ERROR! No argument passed to command module'));
+    commander.help();
+    process.exit(1);
+  }
+
+  if (!fs.existsSync(entry)) {
+    console.error(color.red('target file not found~!'));
+    process.exit(2);
+  }
+
+  const extname = commander.extname;
+  let extnames = []
+  if (extname) {
+    extnames = extname.split(',').filter(item => item);
+  }
+
+  const workspace = commander.workspace;
+  const zly = new Zly(entry, workspace, extnames);
+  const root = zly.root;
+  const out = zly.run();
+  let document = ''
+  if (commander.format === 'yaml') {
+    document = Yaml.stringify(out);
+  } else {
+    document = JSON.stringify(out);
+  }
+
+  if (commander.output) {
+    fs.writeFileSync(commander.output, document);
+  } else if (commander.server) {
+    watcher.watch(workspace || root, {
+      interval: 1000,
+      binaryInterval: 2000,
+      alwaysStat: false,
+      depth: 99,
+      awaitWriteFinish: {
+        stabilityThreshold: 2000,
+        pollInterval: 1000
+      },
+    }).on('change', _ => restart(entry, workspace, extnames))
+      .on('ready', _ => restart(entry, workspace, extnames))
+      .on('unlink', _ => restart(entry, workspace, extnames))
+      .on('error', function (err) {
+        console.log('uncatchException', err);
+      });
+  } else {
+    console.log(document);
+  }
+} catch (err) {
+  console.error(color.red(err.message));
+}
+
